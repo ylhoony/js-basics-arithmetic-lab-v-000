5def5b5cbf96866f6ab241ef4634abf112846ac4
Hoon Lee <yl.hoony@gmail.com>
2017-09-27 19:16:57 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -26,7 +26,7 @@ describe('index.js', function () {
   //   it('contains a random number greater than or equal to 0 and less than 20', function () {
   //     expect(randomNumber).to.be.at.least(0).but.below(20);
   //   });
-  // 
+  //
   //   it("invokes 'Math.random()'", function () {
   //     expect(spyOnMathRandom.calledOnce).to.be.true;
   //   });
@@ -35,10 +35,10 @@ describe('index.js', function () {
   // describe('randomInteger', function () {
   //   it("contains the value of 'randomNumber' rounded down to the nearest integer", function () {
   //     expect(Number.isInteger(randomInteger)).to.be.true;
-  // 
+  //
   //     expect(randomInteger).to.be.at.least(0).and.at.most(19);
   //   });
-  // 
+  //
   //   it("invokes 'Math.floor()'", function () {
   //     expect(spyOnMathFloor.calledOnce).to.be.true;
   //   });
